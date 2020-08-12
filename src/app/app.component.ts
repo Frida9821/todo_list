4a96a3a1f5373ff2b7021845b4dffd96fb5ab3cc
Frida Frasco <frasco.frida@cuallisys.com>
2020-08-11 23:07:07 -0500

    newitem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+class toDo{
+  description: string;
+  done: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+
+  public list: toDo[] = [];
+  public newItem: string;
+
+  addNewtoDo(): void{
+    this.list.push({
+      description: this.newItem,
+      done: false
+    });
+    this.newItem = null;
+  }
 }
